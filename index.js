71c229fb2cfe7ee20cdee44c9fa9205b53b55b68
unknown <abdullahalomar048@gmail.com>
2022-05-29 21:25:21 +0600

    api created

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 
         app.get('/reviews', async (req, res) => {
             const query = {};
-            const cursor = reviewCollection.find(query);
+            const cursor = productCollection.find(query);
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
