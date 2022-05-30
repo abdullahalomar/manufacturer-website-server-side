65e010e1121d43a6f09f6be8f2a007ec92613118
unknown <abdullahalomar048@gmail.com>
2022-05-30 16:35:20 +0600

    jwt generate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
